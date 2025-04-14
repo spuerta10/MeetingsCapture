7554be581bf768e7a921fed579f225273fbebeca
spuerta10 <puertasantiago2002@gmail.com>
2025-04-14 13:32:16 -0500

    fix: Fixed secrets handling via streamlit

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-from pyconfparser import ConfigFactory
+from streamlit import secrets
 
 from src.app.streamlit_app import StreamlitApp
 from src.service.meetings_service import MeetingsService
 
 if __name__ == "__main__":
-    config = ConfigFactory.get_conf(r"notebooks/conf/config.json")
+    config = secrets["general"]
     service: MeetingsService = MeetingsService(
-        gemini_api_secret=config.gemini_api_key, model_type=config.model_type, prompt=config.prompt
+        gemini_api_secret=config["gemini_api_key"],
+        model_type=config["model_type"],
+        prompt=config["prompt"],
     )
     app = StreamlitApp(service=service)
     app.run()
