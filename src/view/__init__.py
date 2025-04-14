5fe548a9362d1ed47591f400c87ba735ca987a29
spuerta10 <puertasantiago2002@gmail.com>
2025-04-14 13:08:25 -0500

    chore: Added init modules files

---
+++ src/view/__init__.py
@@ -0,0 +1,13 @@
+from src.view.images.images_preview_view import ImagesPreviewView
+from src.view.images.images_upload_view import ImagesUploadView
+from src.view.meetings.meetings_download_view import MeetingsDownloadView
+from src.view.meetings.meetings_preview_view import MeetingsPreviewView
+from src.view.meetings.meetings_stats_view import MeetingsStatsView
+
+__all__ = [
+    "ImagesPreviewView",
+    "ImagesUploadView",
+    "MeetingsDownloadView",
+    "MeetingsPreviewView",
+    "MeetingsStatsView",
+]
